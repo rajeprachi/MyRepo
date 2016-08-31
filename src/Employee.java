c6c7ba877ebaefc29a115e85e3831bb7db2ed2a4
Joshi <prachi.r.joshi@accenture.com>
2016-08-31 15:24:52 +0530

    new class added

---
+++ src/Employee.java
@@ -0,0 +1,36 @@
+
+public class Employee {
+	int empId;
+	String name;
+	double salary;
+	public Employee() {	}
+	public Employee(int empId, String name, double salary) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.salary = salary;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
